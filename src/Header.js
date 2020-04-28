ba234a436d585b19b99b9b214c220eccea710f85
abhinav anshul <abhinavanshul4u@gmail.com>
2020-04-28 13:07:00 +0530

    adds metamask login functionality in Header component

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,26 @@
 /* eslint-disable prettier/prettier */
 
 import React from 'react'
+import Wallet from './Wallet'
 
 function Header() {
   return (
-    <div
-      css={`
-        box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 3px;
-        background: rgb(255, 255, 255);
-        padding: 20px;
-        position: relative;
-        z-index: 3;
-      `}
-    >
-      GitFunded
-    </div>
+    <>
+      <div
+        css={`
+          box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 3px;
+          background: rgb(255, 255, 255);
+          padding: 20px;
+          position: relative;
+          z-index: 3;
+        `}
+      >
+        GitFunded
+      </div>
+      <div>
+        <Wallet />
+      </div>
+    </>
   )
 }
 
