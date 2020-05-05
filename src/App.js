041493de9153373e54ed0419a265e9619acee609
abhinav anshul <abhinavanshul4u@gmail.com>
2020-05-05 12:14:57 +0530

    Adds Github API Button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 
 import React from 'react'
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Main } from '@aragon/ui'
 
 import theme from './theme-court'
@@ -22,9 +22,9 @@ function App() {
       <BrowserRouter>
         <Main layout={false} theme={theme}>
           <MainView>
-            <Redirect from='/' to='/dashboard' />
+            {/* <Redirect from='/' to='/dashboard' /> */}
             <Switch>
-              <Route path='/dashboard' component={Dashboard} />
+              <Route exact path='/dashboard' component={Dashboard} />
               <Route exact path='/tasks' component={Tasks} />
               <Route exact path='/disputes' component={Disputes} />
 
--- src/components/Dashboard/Import.js
+++ src/components/Dashboard/Import.js
@@ -1,27 +1,36 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react'
-import { Button, SidePanel } from '@aragon/ui'
+import React, { useEffect } from 'react'
+import { Button } from '@aragon/ui'
 
 function Import() {
-  const [opened, setOpened] = useState(false)
+  const CLIENT_ID = '69bc88033c4b1bc2b4dc'
+  const REDIRECT_URI = 'http://localhost:3000/dashboard'
 
-  const closePanel = () => {
-    setOpened(false)
-  }
+  // const [token, setToken] = useState(null)
 
-  const openPanel = () => {
-    setOpened(true)
-  }
+  useEffect(() => {
+    // console.log('click')
+    const code =
+      window.location.href.match(/\?code=(.*)/) &&
+      window.location.href.match(/\?code=(.*)/)[1]
+    console.log(window.location.href)
+
+    console.log(code)
+
+    fetch(`https://gitfund-oauth.herokuapp.com/authenticate/${code}`)
+      .then((response) => response.json())
+      .then(({ token }) => {
+        console.log(token)
+      })
+  }, [])
 
   return (
     <>
-      <Button mode='strong' onClick={openPanel}>
-        Import
-      </Button>
-
-      <SidePanel onClose={closePanel} title='GITHUB' opened={opened}>
-        Please select repository
-      </SidePanel>
+      <a
+        href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
+      >
+        <Button mode='primary'>Import</Button>
+      </a>
     </>
   )
 }
